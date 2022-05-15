ee25c172353f8b4e2c5e267631095fd78ae667a7
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2022-05-15 12:20:41 +0300

    Generated vertex-registry app

---
+++ vertex-registry/src/main/java/com/ceratipa/vertexregistry/VertexRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.ceratipa.vertexregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertexRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertexRegistryApplication.class, args);
+	}
+
+}
